0e4abdd1a86e2760606e33021df2164c92041e03
imaneroucdhi <imane.rcdi@gmail.com>
2023-02-21 14:35:25 +0100

    CDR-5 <Add new Article using RelationShip>

---
+++ app/Http/Controllers/api/ArticleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Validator;
+
+
+class ArticleController extends Controller
+{
+    
+    public function index (){
+
+        
+        
+    }
+    public function create(Request $request){
+        $validator =Validator::make($request->all(), [
+            'title'=>'required|max:255',
+
+            'description' =>'required ',
+            'content'=>'required ',
+            'category_id'=>'required ',
+            'user_id'=>'required '
+            
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'validation errors',
+                'errors' =>$validator->messages()
+            ],422);
+        }
+        $article = Article::create([
+            'title'=>$request->title,
+            'description'=>$request->description,
+            'content'=>$request->content,
+            'category_id'=>$request->category_id,
+            'user_id'=>$request->user_id
+        ]);
+        $article->load('category','user');// relation
+        return response()->json([
+            'message'=>'Article Seved Successfuly',
+            'data'=>$article
+        ],200);
+    }
+
+    public function list(){
+         $listArticle = Article::with(['user','category']);
+         $list = $listArticle->get();
+         return response()->json([
+            'mesage'=>" All Articles ",
+            'data'  => $list
+         ] , 200);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
