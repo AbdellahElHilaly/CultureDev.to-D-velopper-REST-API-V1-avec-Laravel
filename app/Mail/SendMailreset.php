ab1efc96f018b5f6a74279f10bfb5bdb1a306e28
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-22 15:03:48 +0100

    CDR-3 Add SendMailrest class - completed

--- app/Mail/SendMailreset.php
+++ app/Mail/SendMailreset.php
@@ -13,12 +13,16 @@ class SendMailreset extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $token;
+    public $email;
+
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct($token, $email)
     {
-        //
+        $this->token = $token;
+        $this->email = $email;
     }
 
     /**
@@ -38,6 +42,10 @@ class SendMailreset extends Mailable
     {
         return new Content(
             markdown: 'Email.passwordReset',
+            with: [
+                'token' => $this->token,
+                'email' => $this->email
+            ]
         );
     }
 
