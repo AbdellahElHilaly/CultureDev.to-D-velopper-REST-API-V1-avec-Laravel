185ad4e036214d1c1db459e8e0766c2e1cfd4351
abdellah el hilaly <abdellah.elhilaly.96@gmail.com>
2023-02-24 21:16:34 +0100

    abdellah + imane + hisham : done

--- app/Http/Resources/ArticleResource.php
+++ app/Http/Resources/ArticleResource.php
@@ -25,59 +25,7 @@ class ArticleResource extends JsonResource
 
 
 
-    
-    // public function toArray($request)
-    // {
 
-    //     $data = [
-    //         'id' => $this->id,
-    //         'user' => $this->user->name,
-    //         'title' => $this->title,
-    //         'description' => $this->description,
-    //         'content' => $this->content,
-    //         'category' => $this->category,
-    //         'comment' => $this->comments,
-    //     ];
 
-    //     if ($this->tags->isNotEmpty()) {
-    //         $data['tags'] = $this->tags->pluck('name')->toArray();
-    //     }
-
-    //     return $data;
-    // }
 }
 
-/*
-
-{
-    "status": 201,
-    "message": "Article created successfully",
-    "data": {
-        "id": 13,
-        "user": "autheur",
-        "title": "article title",
-        "description": "article description",
-        "content": "article content",
-        "category": {
-            "name": "Category name",
-        },
-        "comments": [
-            {
-                "user" : "user name",
-                "comment" : "comment body",
-            }
-            {
-                "user" : "user name",
-                "comment" : "comment body",
-            }
-        ],
-
-        "tags": [
-            "adfrk",
-            "cdcz"
-        ]
-    }
-}
-
-
-*/
